f82ba9b13962cdf9723f0f10571b26051e915e5a
Vo Tuan Trung <trungk18@hotmail.com>
2016-09-26 00:32:56 +0800

    Update project structure and readme

---
+++ README.md
@@ -0,0 +1,36 @@
+﻿# [Front-end Boilerplate](https://github.com/trungk18/front-end-boilerplate)
+
+Front-end Boilerplate is a basic structure for every project integrated with Bower, NPM and Gulp for task runner.
+
+* Source: [https://github.com/trungk18/front-end-boilerplate](https://github.com/trungk18/front-end-boilerplate)
+
+## Quick start
+
+* Clone the git repo — `git clone https://github.com/trungk18/front-end-boilerplate.git`
+* Run `npm install` for installing all the dependencies.
+* You might wish to install [Bourbon](http://bourbon.io/) in app/scss folder for writing CSS faster and easier.
+
+## Features
+
+* HTML5 ready. Use the new elements with confidence.
+* Includes:
+  * [Bower](https://bower.io/) for front-end package manager.
+  * [NPM](https://npmjs.com/) for development package manager.
+	* [Gulp](http://gulpjs.com/) for automate and enhance the development flow with pre-defined some neccessary tasks
+		* Compile SASS to CSS (`gulp sass`)
+		* Live-reloading with Browser Sync (`gulp watch`)
+		* Optimizing CSS and JavaScript files (`gulp useref`)
+		* Optimizing Images (`gulp images`)
+		* Cleaning up generated files automatically (`gulp dist:clear`)
+
+## Browser support
+
+* Chrome *(latest 2)*
+* Edge *(latest 2)*
+* Firefox *(latest 2)*
+* Internet Explorer 8+
+* Opera *(latest 2)*
+* Safari *(latest 2)*
+
+*This doesn't mean that HTML5 Boilerplate cannot be used in older browsers, just that we'll ensure compatibility with the ones mentioned above.*
+
---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+//require sass plugin
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var cache = require('gulp-cache');
+var del = require('del');
+var runSequence = require('run-sequence');
+
+//test task
+gulp.task('hello', function () {
+  console.log('Vo Tuan Trung - Front-end Developer');
+});
+
+//SCSS compiler task
+gulp.task('sass', function () {
+  return gulp.src('app/scss/styles.scss')
+    .pipe(sass()) // converts SCSS to CSS with gulp-sass
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+//browser sync task
+gulp.task('browserSync', function () {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
+//concatenate file
+gulp.task('useref', function () {
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('images', function () {
+  return gulp.src('app/images/**/*.+(png|jpg|gif|svg)')
+  .pipe(cache(imagemin()))
+  .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('fonts', function () {
+  return gulp.src('app/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+})
+
+gulp.task('dist:clear', function () {
+  return del.sync('dist');
+})
+
+gulp.task('cache:clear', function (callback) {
+  return cache.clearAll();
+})
+
+gulp.task('watch', ['browserSync', 'sass'], function () {
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  // reloads the browser whenever HTML or JS files change
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+})
+
+//dev task
+gulp.task('default', function (callback) {
+  runSequence(['hello', 'sass', 'browserSync', 'watch'], callback)
+})
+
+//distribution task
+gulp.task('build', function (callback) {
+  runSequence('dist:clean', ['sass', 'useref', 'images', 'fonts'], callback)
+})
